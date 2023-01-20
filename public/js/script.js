4560c0f7253bea7a10584f15374593eeda4aa1a0
Leon Kuijf <leon.kuijf@gmail.com>
2023-01-20 09:13:22 +0100

    fade elements in view when in viewport

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// console.log('test');
+/* To Top Button */
 const toTopBtn = document.querySelector('#toTop');
 toTopBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -12,3 +12,29 @@ window.addEventListener('scroll', (e) => {
     if(fromTop > 800) toTopBtn.classList.add('show');
     else toTopBtn.classList.remove('show');
 });
+/*******/
+
+/* Fade in elements when in viewport */
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2
+};
+
+function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // entry.target.classList.replace('fadeOut', 'fadeIn');
+            entry.target.classList.add('fadeIn');
+        } else {
+            // entry.target.classList.replace('fadeIn', 'fadeOut');
+        }
+    });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+const fadeElms = document.querySelectorAll('.contentWrapper .inner');
+console.log(fadeElms.length);
+fadeElms.forEach(el => observer.observe(el));
+/*******/
--- resources/views/sections/fullwidth_bestand.blade.php
+++ resources/views/sections/fullwidth_bestand.blade.php
@@ -1,3 +1,3 @@
-<div class="inner">
+<div class="inner fadeOut">
     @include('sections.bestand')
 </div>
