b0b84449a0578ec466561978f0296c7fbdf979bf
Leon Kuijf <leon.kuijf@gmail.com>
2023-01-30 10:35:22 +0100

    hide elements using JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-/* To Top Button */
+/***** To Top Button *****/
 const toTopBtn = document.querySelector('#toTop');
 toTopBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -12,26 +12,35 @@ window.addEventListener('scroll', (e) => {
     if(fromTop > 800) toTopBtn.classList.add('show');
     else toTopBtn.classList.remove('show');
 });
-/*******/
+/*********************************************/
 
-/* Fade in elements when in viewport */
+/***** Fade in elements when in viewport *****/
+// Beware of user has disabled JS; do not hide elements using CSS
+const fadeElms = document.querySelectorAll('.contentWrapper .inner');
 const observerOptions = {
     root: null,
     threshold: 0.2
 };
-
 function observerCallback(entries, observer) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             // entry.target.classList.replace('fadeOut', 'fadeIn');
-            entry.target.classList.add('fadeIn');
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0px)";
         } else {
             // entry.target.classList.replace('fadeIn', 'fadeOut');
         }
     });
 }
-
-const observer = new IntersectionObserver(observerCallback, observerOptions);
-const fadeElms = document.querySelectorAll('.contentWrapper .inner');
-fadeElms.forEach(el => observer.observe(el));
-/*******/
+fadeElms.forEach(el => {
+    el.style.opacity = 0; // Beware of user has disabled JS; do not hide elements using CSS
+    el.style.transform = "translateY(50px)";
+});
+setTimeout(() => { // using setTimeout for elements that are directly in viewport, so they show the effect
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    fadeElms.forEach(el => {
+        el.style.transition = "opacity 0.7s ease-in, transform 0.7s ease-out";
+        observer.observe(el);
+    });
+}, 1);
+/*************************************************/
